1d44894e4dd966dedb3803e63c8d75d5a00e80af
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-01 22:47:21 +0600

    form data added at database

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 const AddCoffee = () => {
   const handleAddCoffee = (event) => {
     event.preventDefault();
@@ -21,8 +22,28 @@ const AddCoffee = () => {
       details,
       photo,
     };
-
+    // form.reset("");
     console.log(newCoffee);
+    //send data to server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "User added successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
 
     // send data to the server
   };
