8f13d33c0f8dbd32da9ee13d737628ff6686517b
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-01 15:49:59 +0600

    basic code added

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+const router =
+  createBrowserRouter[
+    {
+      path: "/",
+      element: <App></App>
+    },
+    {
+path:'/update',
+element:
+    }
+  ];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+ <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
