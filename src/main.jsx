4c70f4aad6c2a7ffa3e234ff15c44b97617bd4b9
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-01 18:22:51 +0600

    form added for coffee

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-const router =
-  createBrowserRouter[
-    {
-      path: "/",
-      element: <App></App>
-    },
-    {
-path:'/update',
-element:
-    }
-  ];
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/update",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
